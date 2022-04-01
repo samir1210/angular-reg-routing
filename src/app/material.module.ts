e625a3d7bc76b639077c1059c81b596fe696435e
samir1210 <102782080+samir1210@users.noreply.github.com>
2022-04-01 11:43:53 +0530

    Added Material Module

---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from  '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+ 
+@NgModule({
+imports: [FormsModule,],
+ 
+exports: [FormsModule] 
+})
+ 
+export  class  MaterialModule { }
